155f991ae83ac509cd56f8f16bc2c9fb3585c23a
Sainz31 <122534761+Sainz31@users.noreply.github.com>
2023-02-21 13:53:59 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+let Y = 0
+let X = 0
+basic.forever(function () {
+    Y = input.acceleration(Dimension.Y)
+    X = input.acceleration(Dimension.X)
+    if (X < 0) {
+    	
+    } else {
+    	
+    }
+    if (Y < 0) {
+    	
+    } else {
+    	
+    }
+})
